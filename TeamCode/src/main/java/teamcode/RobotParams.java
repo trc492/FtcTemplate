cf342f5e38a4b83f312d6bb1552c6a63be169ce4
Titan Robotics Club <trc492@users.noreply.github.com>
2024-09-14 23:04:40 -0700

    Updated ftclib. Separated out the odo wheel scale to per axis.

--- TeamCode/src/main/java/teamcode/RobotParams.java
+++ TeamCode/src/main/java/teamcode/RobotParams.java
@@ -296,7 +296,7 @@ public class RobotParams
             driveMotorNames = new String[] {"lfDriveMotor", "rfDriveMotor"};
             driveMotorInverted = new boolean[] {true, false};
             // Odometry Wheels
-            odWheelScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;    // 0.00105687652708656383937269814237 in/count
+            odWheelXScale = odWheelYScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;
             xOdWheelIndices = new int[] {FtcRobotDrive.INDEX_RIGHT_BACK};
             xOdWheelXOffsets = new double[] {0.0};
             xOdWheelYOffsets = new double[] {-168.0 * TrcUtil.INCHES_PER_MM};
@@ -361,7 +361,7 @@ public class RobotParams
             driveMotorNames = new String[] {"lfDriveMotor", "rfDriveMotor", "lbDriveMotor", "rbDriveMotor"};
             driveMotorInverted = new boolean[] {true, false, true, false};
             // Odometry Wheels
-            odWheelScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;    // 0.00105687652708656383937269814237 in/count
+            odWheelXScale = odWheelYScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;
             xOdWheelIndices = new int[] {FtcRobotDrive.INDEX_RIGHT_BACK};
             xOdWheelXOffsets = new double[] {0.0};
             xOdWheelYOffsets = new double[] {-168.0 * TrcUtil.INCHES_PER_MM};
@@ -430,7 +430,7 @@ public class RobotParams
             driveMotorNames = new String[] {"lfDriveMotor", "rfDriveMotor", "lbDriveMotor", "rbDriveMotor"};
             driveMotorInverted = new boolean[] {true, false, true, false};
             // Odometry Wheels
-            odWheelScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;    // 0.00105687652708656383937269814237 in/count
+            odWheelXScale = odWheelYScale = Math.PI * ODWHEEL_DIAMETER / ODWHEEL_CPR;
             xOdWheelIndices = new int[] {FtcRobotDrive.INDEX_RIGHT_BACK};
             xOdWheelXOffsets = new double[] {0.0};
             xOdWheelYOffsets = new double[] {-168.0 * TrcUtil.INCHES_PER_MM};
